97091ff704b8abe0bd14b0919d4a4f9e8c27ec51
Joe Ed Mialen <joeed.mialen@gmail.com>
2021-01-12 11:52:01 +0800

    Update grammar and footer

--- js/footnote.js
+++ js/footnote.js
@@ -1,7 +1,7 @@
 					
 var newHTMLfootnote = `<div>
 					<p>
-				      <p>Website Developed and Maintained By DEPED LA CASTELLANA 1 ICT OFFICERS</p>
+				      <p>Website Developed and Maintained By Sir Joe Ed T. Mialen, Licensed Educator, DepEd Phil.</p>
 				      <p>automatedecd.github.io | Powered by Github Pages</p>
 				      <p>All Rights Reserved 2020</p>
 					
